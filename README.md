0ef1c9a7fc1f957a0a5f4d5876a9157c70a494ab
deternan <chke@iir.csie.ncku.edu.tw>
2017-06-06 22:51:24 +0800

    weather bot

---
+++ README.md
@@ -0,0 +1,31 @@
+# The Messaging API SDK for Java
+
+
+ 
+### Requirements
+
+- Gradle: https://spring.io/guides/gs/gradle/
+- heroku:   
+直接建立一個 heroku
+https://elements.heroku.com/buttons/line/line-bot-sdk-java
+ 
+將 連接 GitHub
+https://devcenter.heroku.com/articles/github-integration
+ 
+目的: code 同步到 git 後heroku會自動deployed
+ 
+## How to build this bot
+修改
+
+LINE_CHANNEL_SECRET = 'LINE_CHANNEL_SECRET'
+
+LINE_CHANNEL_ACCESS_TOKEN = 'LINE_CHANNEL_ACCESS_TOKEN'
+ 
+Reference
+http://www.oxxostudio.tw/articles/201701/line-bot.html
+http://www.evanlin.com/create-your-line-bot-golang/
+https://github.com/cslin0915/hello-line-bot
+
+
+
+
---
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.example.bot.spring.echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@SpringBootApplication
+@LineMessageHandler
+public class EchoApplication 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApplication.class, args);
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+		// CJKV check
+		String get_return = CJKV_check(event.getMessage().getText());
+		return new TextMessage(get_return);
+        //return new TextMessage("Auto:  "+event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+	
+	public String CJKV_check(String input_str)
+	{
+		boolean check;
+		String return_str = "";
+		check = input_str.codePoints().anyMatch(codepoint ->
+	            Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HAN);
+		
+		//System.out.println(check);
+		if(check == true){
+			return_str = "Non English";
+		}else{
+			return_str = "English";
+		}
+		
+		return return_str;
+	}
+}
